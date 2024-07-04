75c021ee51ea9811c42b8d0a0f079c9c185617cd
Ginger <gingerginger5cc@gmail.com>
2024-07-03 23:55:38 -0400

    ex4.8 to 4.14 - add different tests to backend by using supertest

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,45 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-blogsRouter.post('/', (request, response) => {
-const blog = new Blog(request.body)
 
-blog
-    .save()
-    .then(result => {
-    response.status(201).json(result)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs  = await Blog.find({})
+  response.json(blogs)
 })
-  
+
+
+blogsRouter.post('/', async(request, response) => {
+  const body = new Blog(request.body)
+  const blog = new Blog ({
+    title: body.title,
+    autor: body.author || 'default author',
+    url: body.url,
+    likes: body.likes || 0,
+  })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async(request, response) => {
+  const delBlog = await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+
+blogsRouter.put('/:id', async(request, response) => {
+  const body = request.body
+  const upblog = {
+    title: body.title,
+    author: body.author || 'default author',
+    url: body.url,
+    likes: body.likes || 0,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, upblog, { new: true })
+  response.status(200).json(updatedBlog)
+
+})
+
   
 
 
---
+++ tests/test_helper.js
@@ -0,0 +1,45 @@
+const Blog = require('../models/blog')
+const initialBlog = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+    },
+    {
+        title: "Overreacting",
+        author: "You know who",
+        url: "https://overreacting.com/",
+        likes: 10,
+    },
+    {
+        title: "Promises do not always work",
+        author: "Happy Pan",
+        url: "http://promisesuse.html",
+        likes: 3,
+    },
+  ]
+
+
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlog, nonExistingId, blogsInDb
+}
