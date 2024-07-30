de2a64f5899c7f1612ce36f736dcd9cce8f651b9
Ginger <gingerginger5cc@gmail.com>
2024-07-30 11:02:50 -0400

    fix problems for not showing username after adding new blogs

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const commentsRouter = require('./controllers/comments')
 
 
 
@@ -41,6 +42,7 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/comments', commentsRouter)
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
